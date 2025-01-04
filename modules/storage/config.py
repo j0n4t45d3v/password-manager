3e0d6f154a7bda5592bec54f43935a2e6ce00b51
Jonatas <delimajonatas875@gmail.com>
2025-01-04 11:50:18 -0300

    feat: create file if not exists in system

--- modules/storage/config.py
+++ modules/storage/config.py
@@ -1,5 +1,11 @@
 import os
+import json
 
 _BASE_USER_PATH = os.path.expanduser("~")
 LOGINS_FILE = os.path.join(_BASE_USER_PATH, ".logins.json")
 
+
+def init_config_if_not_configured() -> None:
+    if not os.path.exists(LOGINS_FILE):
+        with open(LOGINS_FILE, "w") as storage_file:
+            json.dump({}, storage_file, indent=4)
